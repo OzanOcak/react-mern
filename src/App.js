5b86072d08f8c199b0337e30b531db5277ae3911
OzanOcak <ozanocak@gmail.com>
2022-09-03 22:02:55 -0400

    git auth token login frontend

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ import DashLayout from "./components/DashLayout";
 import Welcome from "./features/auth/Welcome";
 import NotesList from "./features/notes/NotesList";
 import UsersList from "./features/users/UsersList";
+import Prefetch from "./features/auth/Prefetch";
+import EditNote from "./features/notes/EditNote";
+import NewNote from "./features/notes/NewNote";
+import EditUser from "./features/users/EditUser";
+import NewUserForm from "./features/users/NewUserForm";
 
 function App() {
   return (
@@ -13,19 +18,25 @@ function App() {
       <Route path="/" element={<Layout />}>
         <Route index element={<Public />} />
         <Route path="login" element={<Login />} />
+        <Route element={<Prefetch />}>
+          <Route path="dash" element={<DashLayout />}>
+            <Route index element={<Welcome />} />
 
-        <Route path="dash" element={<DashLayout />}>
-          <Route index element={<Welcome />} />
+            <Route path="notes">
+              <Route index element={<NotesList />} />
+              <Route path=":id" element={<EditNote />} />
+              <Route path="new" element={<NewNote />} />
+            </Route>
 
-          <Route path="notes">
-            <Route index element={<NotesList />} />
+            <Route path="users">
+              <Route index element={<UsersList />} />
+              <Route path=":id" element={<EditUser />} />
+              <Route path="new" element={<NewUserForm />} />
+            </Route>
           </Route>
-
-          <Route path="users">
-            <Route index element={<UsersList />} />
-          </Route>
+          {/* End Dash */}
         </Route>
-        {/* End Dash */}
+        {/** end of prefetch */}
       </Route>
     </Routes>
   );
